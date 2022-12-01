88ac0f0c96d3e582a875e6b300fe396d200f0091
ngothilinh20187179 <ngothilinh12915@gmail.com>
2022-12-01 09:18:41 +0700

    init ball

---
+++ src/ball.py
@@ -0,0 +1,15 @@
+import pygame
+
+from src import color
+
+
+class Ball:
+    MAX_VEL = 5
+    COLOR = color.WHITE
+
+    def __init__(self, x, y, radius):
+        self.x = self.original_x = x
+        self.y = self.original_y = y
+        self.radius = radius
+        self.x_vel = self.MAX_VEL
+        self.y_vel = 0
