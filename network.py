4de9477dee1281b3bf1a6b8aff8fb8bf900da24b
LeHong0101 <93897554+LeHong0101@users.noreply.github.com>
2022-12-08 10:03:17 +0700

    Update network.py

--- network.py
+++ network.py
@@ -1,1 +1,19 @@
 
+import socket
+import pickle
+
+
+class Network:
+       
+
+    def get_player(self):
+        return self.player
+
+
+    def send(self, data):
+        try:
+            self.client.send(pickle.dumps(data))
+            reply = pickle.loads(self.client.recv(4096 * 2))
+            return reply
+        except socket.error as e:
+            return str(e)
