772f2faf4cf3a35263b5a4f89c5a3f0b49025837
DongNM <dongm241@gmail.com>
2023-01-04 16:28:02 +0700

    network: init values

--- network.py
+++ network.py
@@ -5,9 +5,20 @@ import pickle
 
 class Network:
        
+    def __init__(self):
+        self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.host = "localhost"
+        self.port = 5555
+        self.addr = (self.host, self.port)
+        self.player = self.connect()
+
 
     def get_player(self):
         return self.player
+
+
+    def connect(self):
+        pass
 
 
     def send(self, data):
