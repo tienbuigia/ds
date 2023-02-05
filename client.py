2af8ddd700d3e427eb4fd23773aaebe2c8eb6d0f
tien <chad@tienbuigia.xyz>
2023-02-05 13:17:56 +0700

    press escape to exist

--- client.py
+++ client.py
@@ -40,13 +40,14 @@ def main():
     n = Network()
     player = n.get_player()
     clock = pygame.time.Clock()
+    keys = pygame.key.get_pressed()
 
     while run:
         clock.tick(FPS)
         player2, ball, score = n.send(player)
 
         for event in pygame.event.get():
-            if event.type == pygame.QUIT:
+            if event.type == pygame.QUIT or keys[pygame.K_ESCAPE]:
                 run = False
                 pygame.quit()
 
